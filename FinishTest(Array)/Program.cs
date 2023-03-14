753ed21dfef4d8d0cac23f9ebc8e19dcaebf9e6e
Sergey <sergey.kurilov@mail.ru>
2023-03-14 16:44:50 +0300

    done

---
+++ FinishTest(Array)/Program.cs
@@ -0,0 +1,32 @@
+﻿string[] arr = {"hello", "2", "world", ":-)" , "123", "slo"};
+string[] array = new string[arr.Length];
+
+
+
+
+void CalculateArray (string[] arr,string[] array)
+{
+    int count = 0;
+
+    for ( int i = 0 ; i < arr.Length; i ++)
+    {
+        if (arr[i].Length<=3 )
+        {
+            array[count] = arr[i];
+            count++;
+            Console.Write(array[count]);
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+CalculateArray(arr, array);
+
+Console.Write("Итоговый массив: ");
+PrintArray(array);
